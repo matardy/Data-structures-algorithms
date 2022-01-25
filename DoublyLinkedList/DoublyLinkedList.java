46df8a7b550efb8d581682beea4ffc6defca0409
Gutemberg S. Mendoza <gutembergmendoza@Gutembergs-MacBook-Pro.local>
2022-01-25 12:15:07 -0500

    Last update

--- DoublyLinkedList/DoublyLinkedList.java
+++ DoublyLinkedList/DoublyLinkedList.java
@@ -74,6 +74,50 @@ public class DoublyLinkedList <E>{
         return remove(trailer.getPrev());
     }
 
+    // modifed methods
+
+    public void insertNth(E e, int position){
+        Node <E> node = new Node<>(e, null,null);
+
+        if(header.next == null){
+            // Lista vacia
+            if(position != 0){
+                return;
+            } else { // posicion en cero, con head == null
+                header.next = node;
+            }
+        }
+
+        //No editar variables de instancia
+        // Si se ingresa una posicion cero, es decir addFirst()
+        if(header.next != null && position == 0){
+            addBetween(e, header,header.getNext()); // Este metodo ya aumenta uno al tamanio
+
+            return;
+        }
+
+        Node<E> current = header.next;
+
+        int i = 0;
+        while(i < position){
+            current = current.next; // Nodo que se encuentra en la posicion a insertar
+            i++;
+        }
+
+        if(position!=0){
+            //Conecto el nuevo nodo
+            node.setPrev(current.prev);
+            node.setNext(current);
+            //Actualizo los punteros
+            current.prev.setNext(node);
+            current.next.setPrev(node);
+            size++;
+
+        }
+
+
+    }
+
     // metodos priv
 
     private void addBetween(E e, Node<E> prev, Node<E> next){
@@ -113,6 +157,16 @@ public class DoublyLinkedList <E>{
         myList.addLast(3);
         myList.addLast(8);
         myList.show();
+        System.out.println("\nSize: " + myList.size());
+        myList.insertNth(24,2);
+        System.out.println("Nueva lista: ");
+        myList.show();
+        System.out.println("\nSize: " + myList.size());
+        myList.insertNth(7,0);
+        System.out.println("Nueva lista: ");
+        myList.show();
+        System.out.println("\nSize: " + myList.size());
+
 
 
 
